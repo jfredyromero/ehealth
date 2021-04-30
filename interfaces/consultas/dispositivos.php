dd80f415eb7bca6a969cee9a5c13a2f4e82f328e
Lina Virginia <linavm@unicauca.edu.co>
2021-04-29 21:31:26 -0500

    Cambios interfaces con ubicacion

--- interfaces/consultas/dispositivos.php
+++ interfaces/consultas/dispositivos.php
@@ -28,14 +28,14 @@
             ?>
 
             <div id="page-content">
-                <table width="80%" align=center cellpadding=5 border=1>
+                <table width="80%" align=center cellpadding=7 border=1>
                     <tr>
-                        <td valign="center" align=center width=80% colspan=6>
+                        <td valign="center" align=center width=80% colspan=7>
                             <img src="/ehealth/static/img/logo.png" width=800 height=250>
                         </td>
                     </tr>
                     <tr>
-                        <td valign="center" align=center width=80% colspan=6 bgcolor="#281E5D">
+                        <td valign="center" align=center width=80% colspan=7 bgcolor="#281E5D">
                             <h1>
                                 <font color=white>Consulta de dispositivos eHealth</font>
                             </h1>
@@ -75,7 +75,7 @@
                         <form method="GET">
                             <td style="border: none;" valign="center" align=center colspan=3>
                             </td>
-                            <td style="border: none;" valign="center" align=right colspan=1>
+                            <td style="border: none;" valign="center" align=right colspan=2>
                                 <input type="number" class="form-control" name="id_tarjeta" placeholder="ID de la tarjeta..." required>
                             </td>
                             <td style="border: none;" valign="center" align=center colspan=2>
@@ -87,7 +87,7 @@
                     </tr>
                     <tr height=50>
                         <td valign="center" align=center bgcolor="#E1E1E1">
-                            <b>ID de la Tarjeta</b>
+                            <b>ID</b>
                         </td>
                         <td valign="center" align=center bgcolor="#E1E1E1">
                             <b>Propietario</b>
@@ -99,9 +99,12 @@
                             <b>Estado</b>
                         </td>
                         <td valign="center" align=center bgcolor="#E1E1E1">
-                            <b>Último registro</b>
+                            <b>Fecha último registro</b>
                         </td>
-                        <td valign="center" align=center bgcolor="#E1E1E1" width=80px>
+                        <td valign="center" align=center bgcolor="#E1E1E1">
+                            <b>Hora último registro</b>
+                        </td>
+                        <td valign="center" align=center bgcolor="#E1E1E1">
                             <b>Editar</b>
                         </td>
                     </tr>
@@ -112,10 +115,17 @@
                     $sql1 = "SELECT * from datos_dispositivos order by id_tarjeta"; // Aqu� se ingresa el valor recibido a la base de datos.
                     $result1 = $mysqli->query($sql1);
                     if (isset($_GET["submit"]) && !empty($_GET["submit"])) {
+                        //Busca fehca máxima
                         $sql2 = "SELECT MAX(fecha) from datos_medidos WHERE id_tarjeta= $id";
                         $result2 = $mysqli->query($sql2);
                         $row2 = $result2->fetch_array(MYSQLI_NUM);
                         $fecha = $row2[0];
+                        //Busca hora máxima
+                        $sql23 = "SELECT MAX(hora) from datos_medidos WHERE id_tarjeta= $id AND fecha='$fecha'";
+                        $result23 = $mysqli->query($sql23);
+                        $row23 = $result23->fetch_array(MYSQLI_NUM);
+                        $hora = $row23[0];
+
                         $sql3 = "SELECT * from datos_dispositivos WHERE id_tarjeta= $id";
                         $result3 = $mysqli->query($sql3);
                         while ($row3 = $result3->fetch_array(MYSQLI_NUM)) {
@@ -132,7 +142,10 @@
                                 <?php echo $propietario; ?>
                             </td>
                             <td valign="center" align=center>
-                                <?php echo $ubicacion; ?>
+                                <?php //echo $ubicacion; ?>
+                                <a class="btn btn-lg btn-block" href="/ehealth/interfaces/consultas/ubicacion.php?id_tarjeta=<?php echo $id; ?>" role="button">
+                                <i class="fas fa-map-marked-alt"></i>
+                                </a>
                             </td>
                             <td valign="center" align=center>
                                 <?php
@@ -151,7 +164,10 @@
                                 <?php echo $fecha; ?>
                             </td>
                             <td valign="center" align=center>
-                                <a class="btn btn-lg btn-block" href="/ehealth/interfaces/dispositivos/editar.php?id_tarjeta=<?php echo $ID_TARJ; ?>" role="button">
+                                <?php echo $hora; ?>
+                            </td>
+                            <td valign="center" align=center>
+                                <a class="btn btn-lg btn-block" href="/ehealth/interfaces/dispositivos/editar.php?id_tarjeta=<?php echo $id; ?>" role="button">
                                     <img src="/ehealth/static/img/dibujar.png" width=32 height=32>
                                 </a>
                             </td>
@@ -159,13 +175,13 @@
                         <tr height=20>
                         </tr>
                         <tr>
-                            <td style="border: none;" valign="center" align=left colspan=4>
+                            <td style="border: none;" valign="center" align=left colspan=6>
                                 <a class="btn btn-lg" style="background-color:#281E5D; color:white" href="/ehealth/interfaces/consultas/dispositivos.php" role="button">
                                     <i class="fas fa-angle-double-left"></i>
                                     <span class="pl-3">Volver</span>
                                 </a>
                             </td>
-                            <td style="border: none;" valign="center" align=right colspan=2>
+                            <td style="border: none;" valign="center" align=right colspan=4>
                                 <a class="btn btn-lg" style="background-color:#281E5D; color:white" href="/ehealth/interfaces/dispositivos/añadir.php" role="button">
                                     <i class="fas fa-plus-circle"></i>
                                     <span class="pl-3">Nuevo</span>
@@ -187,6 +203,10 @@
                             $result2 = $mysqli->query($sql2);
                             $row2 = $result2->fetch_array(MYSQLI_NUM);
                             $fecha = $row2[0];
+                            $sql23 = "SELECT MAX(hora) from datos_medidos WHERE id_tarjeta= $contador AND fecha='$fecha'";
+                            $result23 = $mysqli->query($sql23);
+                            $row23 = $result23->fetch_array(MYSQLI_NUM);
+                            $hora = $row23[0];
                         ?>
                             <tr>
                                 <td valign="center" align=center>
@@ -196,7 +216,10 @@
                                     <?php echo $propietario; ?>
                                 </td>
                                 <td valign="center" align=center>
-                                    <?php echo $ubicacion; ?>
+                                    <?php // echo $ubicacion; ?>
+                                    <a class="btn btn-lg btn-block" href="/ehealth/interfaces/consultas/ubicacion.php?id_tarjeta=<?php echo $ID_TARJ; ?>" role="button">
+                                    <i class="fas fa-map-marked-alt"></i>
+                                    </a>
                                 </td>
                                 <td valign="center" align=center>
                                     <?php
@@ -215,6 +238,9 @@
                                     <?php echo $fecha; ?>
                                 </td>
                                 <td valign="center" align=center>
+                                    <?php echo $hora; ?>
+                                </td>
+                                <td valign="center" align=center>
                                     <a class="btn btn-lg btn-block" href="/ehealth/interfaces/dispositivos/editar.php?id_tarjeta=<?php echo $ID_TARJ; ?>" role="button">
                                         <img src="/ehealth/static/img/dibujar.png" width=32 height=32>
                                     </a>
@@ -228,7 +254,7 @@
                     <tr height=20>
                     </tr>
                     <tr>
-                        <td style="border: none;" valign="center" colspan=4>
+                        <td style="border: none;" valign="center" colspan=5>
                         </td>
                         <td style="border: none;" valign="center" align=right colspan=2>
                             <a class="btn btn-lg" style="background-color:#281E5D; color:white" href="/ehealth/interfaces/dispositivos/añadir.php" role="button">
---
+++ interfaces/consultas/ubicacion.php
@@ -0,0 +1,163 @@
+<?php
+$conexion = $_SERVER['DOCUMENT_ROOT'] . "/ehealth/procesos/conexion.php";
+include $conexion;  // Conexi�n tiene la informaci�n sobre la conexi�n de la base de datos.
+$autenticacion = $_SERVER['DOCUMENT_ROOT'] . "/ehealth/procesos/autenticacion_sesion.php";
+include $autenticacion;
+$validacion = $_SERVER['DOCUMENT_ROOT'] . "/ehealth/procesos/validar_acceso.php";
+include $validacion;
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+    <?php
+    $head = $_SERVER['DOCUMENT_ROOT'] . "/ehealth/static/php/head.php";
+    include $head;
+    ?>
+
+</head>
+
+<body background="/ehealth/static/img/background.jpg">
+    <h1 id="home-title">eHealth: Dispositivo IoT</h1>
+    <div id="home">
+
+        <?php
+        if ($_SESSION["tipo_usuario"] == 1)
+            $nav = $_SERVER['DOCUMENT_ROOT'] . "/ehealth/static/php/nav_admin.php";
+        else
+            $nav = $_SERVER['DOCUMENT_ROOT'] . "/ehealth/static/php/nav_user.php";
+        include $nav;
+        ?>
+
+        <div id="page-content">
+            <table width="80%" align=center cellpadding=3 border=1>
+                <tr>
+                    <td valign="top" align=center width=80& colspan=8>
+                        <img src="/ehealth/static/img/logo.png" width=800 height=250>
+                    </td>
+                </tr>
+                <tr>
+                    <td valign="top" align=center width=80& colspan=8 bgcolor="#281E5D">
+                        <h1>
+                            <font color=white>Ubicación del Dispositivo</font>
+                        </h1>
+                    </td>
+                </tr>
+
+                <?php
+
+                $mysqli = new mysqli($host, $user, $pw, $db);
+                $id_tarj = $_GET['id_tarjeta'];
+                $sql1 = "SELECT * from datos_dispositivos WHERE id_tarjeta= '$id_tarj'";
+                $result1 = $mysqli->query($sql1);
+                $row1 = $result1->fetch_array(MYSQLI_NUM);
+                $id_ta = $row1[0];
+                $estado = $row1[1];
+                $ubicacion = $row1[2];
+                $propietario = $row1[3];
+
+                $sqlubi = "SELECT * from datos_ubicaciones WHERE id_tarj=$id_tarj  order by id DESC"; //CONSULTA LA ULTIMA UBICACION AGREGADA A LA TABLA UBICACIONES
+                $resultubi = $mysqli->query($sqlubi);
+                $rowubi = $resultubi->fetch_array(MYSQLI_NUM);
+
+                if ($rowubi == NULL) {
+                ?>
+                    <tr>
+                        <td valign="center" align=center bgcolor="#E1E1E1" colspan=8>
+                            <b>El ID solicitado NO tiene registro de ubicación</b>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td bgcolor="#EEEEEE" align=center colspan=8>
+                            <a class="btn btn-lg" style="background-color:#281E5D; color:white" href="/ehealth/interfaces/consultas/dispositivos.php" role="button">
+                                <i class="fas fa-angle-double-left"></i>
+                                <span class="pl-3">Volver</span>
+                            </a>
+                        </td>
+                    </tr>
+                <?php
+                } else {
+                    $latitud = $rowubi[1];
+                    $longitud = $rowubi[2];
+                ?>
+                    <form method=POST action="ubicacion.php">
+                        <tr>
+                            <td bgcolor="#CCEECC" align=center colspan=2>
+                                <font FACE="arial" SIZE=2 color="#000044"> <b>ID:</b></font>
+                            </td>
+                            <td bgcolor="#EEEEEE" align=center colspan=6>
+                                <input type="number" value=<?php echo $id_ta; ?> disabled>
+                                <input type="hidden" name="id_tarjeta" value=<?php echo $id_ta; ?>>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td bgcolor="#CCEECC" align=center colspan=2>
+                                <font FACE="arial" SIZE=2 color="#000044"> <b>Propietario</b></font>
+                            </td>
+                            <td bgcolor="#EEEEEE" align=center colspan=6>
+                                <input type="text" value="<?php echo $propietario; ?>" disabled>
+                                <input type="hidden" name="propietario" value=<?php echo $propietario; ?>>
+                            </td>
+                        </tr>
+                        <tr height=20>
+                        </tr>
+                        <tr>
+                            <td valign="center" align=center width=80% colspan=8 bgcolor="#281E5D">
+                                <h1>
+                                    <font color=white>Mapa</font>
+                                </h1>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td valign="center" align=center width=80% colspan=8 bgcolor="#E1E1E1">
+                                <div id="map" style="height:400px;width:90%;">
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td bgcolor="#EEEEEE" align=center colspan=8>
+                                <a class="btn btn-lg" style="background-color:#281E5D; color:white" href="/ehealth/interfaces/consultas/dispositivos.php" role="button">
+                                    <i class="fas fa-angle-double-left"></i>
+                                    <span class="pl-3">Volver</span>
+                                </a>
+                            </td>
+                        </tr>
+                    </form>
+                <?php
+                } ?>
+            </table>
+        </div>
+    </div>
+    <div id="jscripts">
+        <?php
+        $jscripts = $_SERVER['DOCUMENT_ROOT'] . "/ehealth/static/php/jscripts.php";
+        include $jscripts;
+        ?>
+        <script>
+            function initMap() {
+                var latit = <?php echo $latitud ?>;
+                var longi = <?php echo $longitud ?>;
+                var uluru = {
+                    lat: latit,
+                    lng: longi
+                };
+                var map = new google.maps.Map(document.getElementById('map'), {
+                    zoom: 14,
+                    center: uluru
+                });
+                var marker = new google.maps.Marker({
+                    position: uluru,
+                    map: map
+                });
+            }
+        </script>
+        <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCwrROSJLYv3oz-WQeIyNRa37c1U0GXRPQ&callback=initMap">
+            <!-- Se deben reemplazar las XXXX por la API Key de Google MAPS 
+            -->
+        </script>
+    </div>
+</body>
+
+</html>
